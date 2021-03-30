4ac029cf8d61525b0db88377ea21fb132c58f2f3
Lz1y <system@lz1y.cn>
2021-03-30 17:31:11 +0800

    dist source

---
+++ dist/scripts/options.js
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('\'Allo \'Allo! Option');
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHRzL29wdGlvbnMuanMiXSwic291cmNlc0NvbnRlbnQiOlsiJ3VzZSBzdHJpY3QnO1xuXG5jb25zb2xlLmxvZygnXFwnQWxsbyBcXCdBbGxvISBPcHRpb24nKTsiXSwiZmlsZSI6InNjcmlwdHMvb3B0aW9ucy5qcyJ9
